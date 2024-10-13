a37eca9112093d397631da6b302f16c3e5c850fe
Daniel Snytkin <danyas.daniel@gmail.com>
2024-10-12 20:08:35 -0400

    Finished Calculator
    This calculator can do simple operations like addition, subtraction, multiplication and division.

---
+++ Assets/Scripts/CalculatorPrintManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class CalculatorPrintManager : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    public TextMeshProUGUI result;
+    public float number1 = 0f;
+    public float number2 = 0f;
+    public string NumberOperation;
+    
+    //**Print the numbers on the screen text (can be assigned in the button inspector)**//
+    public void PrintButton(string text)
+    {
+        inputField.text += text.ToString();
+    }
+
+    //**Operations**//
+    public void Addition()
+    {
+        number1 = float.Parse(inputField.text);
+        NumberOperation = ("+");
+        inputField.text = "";
+    }
+    public void Subtraction()
+    {
+        number1 = float.Parse(inputField.text);
+        NumberOperation = ("-");
+        inputField.text = "";
+    }
+    public void Division()
+    {
+        number1 = float.Parse(inputField.text);
+        NumberOperation = ("/");
+        inputField.text = "";
+    }
+    public void Multiplication()
+    {
+        number1 = float.Parse(inputField.text);
+        NumberOperation = ("*");
+        inputField.text = "";
+    }
+    //**Remove the numbers we have (backspace but better)**//
+    public void OnClickRemoveButton()
+    {
+        inputField.text = "";
+    }
+
+    //**Print the result**//
+    public void PrintTheResult()
+    {
+        float result = 0f;
+        number2 = float.Parse(inputField.text);
+        string operation = NumberOperation;
+
+        switch(operation)
+        {
+            case "+":
+                result = number1 + number2;
+                inputField.text = result.ToString();
+                break;
+            case "-":
+                result = number1 - number2;
+                inputField.text = result.ToString();
+                break;
+            case "/":
+                result = number1 / number2;
+                inputField.text = result.ToString(); 
+                break;
+            case "*":
+                result = number1 * number2;
+                inputField.text = result.ToString();
+                break;
+        }
+    }
+    
+}
